ba09895cb37c24277672115537e75f4d70ee6c88
Tanvir Alam Sakib <73899287+Tanvir-Sakib@users.noreply.github.com>
2022-10-16 19:54:26 +0600

    Add files via upload
    Demo Site updated

---
+++ ASSR/script.js
@@ -0,0 +1,34 @@
+var nav = document.querySelector('nav');
+
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 100) {
+        nav.classList.add('bg-dark', 'shadow');
+      } else {
+        nav.classList.remove('bg-dark');
+      }
+    });
+
+var swiper = new Swiper(".home-slider", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  loop:true,
+  grabCursor:true,
+});
+var swiper = new Swiper(".home-courses-slider", {
+  loop:true,
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
